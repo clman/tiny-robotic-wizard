12fa7d223be505ae8a0173faea38fadb2d335bd1
hideki.shiro <hideki.shiro@e6a31962-79b9-11de-8cd9-f99136019484>
2009-09-15 15:56:37 +0000

    ブートローダライブラリの組み込み完了！

--- tiny-robotic-wizard/ProgramTransmitter.cs
+++ tiny-robotic-wizard/ProgramTransmitter.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using System.Windows.Forms;
+using AvrLib.Image;
+using HidBootLib;
 
 namespace tiny_robotic_wizard
 {
@@ -14,6 +17,28 @@ namespace tiny_robotic_wizard
             // HEXファイルを生成
             MemoryStream hexStream = new MemoryStream();
             translator.Translate(programCode, hexStream);
+
+            // ブートローダを検索
+            String[] targetDevices = HidBoot.Enumerate();
+            if (targetDevices.Length == 0)
+            {
+                MessageBox.Show("HIDBootデバイスが見つかりませんでした．接続を確認してください．\nユーザーアプリケーションを実行中の場合はリセットボタンを押してください．");
+                return;
+            }
+
+            using (HidBoot hidBoot = new HidBoot(targetDevices[0]))
+            {
+                // ここに転送中のメッセージを表示
+
+                // 転送開始
+                SparseImage spm = HexLoader.LoadIntel(hexStream);
+                hexStream.Close();
+                byte[] prog = spm.ToBlockImage();
+                hidBoot.WriteApplication(prog, spm.MinimumAddress, spm.MaximumAddress);
+                hidBoot.RunApplication();
+
+                // ここに転送完了のメッセージを表示
+            }
         }
     }
 }
