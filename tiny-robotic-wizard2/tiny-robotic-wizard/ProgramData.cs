4d7f50dbbbbda00fb90691575edc9c56df7fd622
hideki.shiro <hideki.shiro@e6a31962-79b9-11de-8cd9-f99136019484>
2009-10-25 18:26:18 +0000

    イベントの連鎖に対応したいが，既存のコードが気持ち悪いので， tiny-robotic-wizard2として新たに作り始める．

---
+++ tiny-robotic-wizard2/tiny-robotic-wizard/ProgramData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tiny_robotic_wizard
+{
+    public class Input : List<int?> { }
+    public class Output : List<int?> { }
+    public class Context : List<Input> { }
+    public class ProgramData : Dictionary<Context, Output>
+    {
+        /// <summary>
+        /// ProgramTemplate
+        /// </summary>
+        public ProgramTemplate ProgramTemplate { get; private set; }
+        /// <summary>
+        /// イベントの連鎖数
+        /// </summary>
+        private int nestLevel = 3;
+        public int NestLevel { get; set; }
+        public ProgramData(ProgramTemplate programTemplate)
+        {
+            this.ProgramTemplate = programTemplate;
+            Context context = new Context();
+            Input input = new Input();
+            Output output = new Output();
+            for (int i = 1; i <= this.ProgramTemplate.Input.Device.Length; i++)
+            {
+                input.Add(null);
+            }
+            for (int i = 1; i <= this.ProgramTemplate.Output.Device.Length; i++)
+            {
+                output.Add(null);
+            }
+            for (int i = 1; i <= this.nestLevel; i++)
+            {
+                context.Add(input);
+            }
+            this.Add(context, output);
+        }
+    }
+}
