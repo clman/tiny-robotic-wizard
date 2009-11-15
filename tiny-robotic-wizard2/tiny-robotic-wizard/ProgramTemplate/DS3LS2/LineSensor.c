eab905debabec1d6b69d459e69ea09ad685a941a
hideki.shiro <hideki.shiro@e6a31962-79b9-11de-8cd9-f99136019484>
2009-11-15 07:17:20 +0000

    ProgramTemplateを再追加

---
+++ tiny-robotic-wizard2/tiny-robotic-wizard/ProgramTemplate/DS3LS2/LineSensor.c
@@ -0,0 +1,19 @@
+#define LSDDR DDRC
+#define LSPORT PINC
+#define LS0 PC4
+#define LS1 PC5
+
+int LineSensor;
+
+void update_LineSensor(void){
+	LineSensor = ((LSPORT&_BV(LS1))?2:0) | ((LSPORT&_BV(LS0))?1:0);
+	return;
+}
+
+void initialize_LineSensor(void){
+	LSDDR &= ~_BV(LS1) & ~_BV(LS0);
+
+	update_LineSensor();
+
+	return;
+}
